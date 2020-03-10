22014d113b0735d2c82413b81dd7efcaa817af0f
Андрей Васильев <puhlikvinni@gmail.com>
2020-03-10 09:00:46 +0300

    add code

--- Solution/HM.02/Program.cs
+++ Solution/HM.02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace HM._02
 {
@@ -11,7 +12,17 @@ namespace HM._02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StreamReader textReader = new StreamReader(@"C:\Temp\image.txt", true);
+            string textReaderResult = textReader.ReadToEnd();
+            string[] arrayOfTextResult = textReaderResult.Split(' ');
+            byte[] imageBytes = new byte[arrayOfTextResult.Length - 1];
+            for (int i = 0; i < arrayOfTextResult.Length - 1; i++)
+            {
+                byte binary = Convert.ToByte(arrayOfTextResult[i], 2);
+                imageBytes[i] = binary;
+            }
+            File.WriteAllBytes(@"C:\Temp\image.png", imageBytes);
+            textReader.Dispose();
         }
     }
 }

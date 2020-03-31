07030a19caa50595eded888871822287b94363a7
Андрей Васильев <puhlikvinni@gmail.com>
2020-03-31 12:13:57 +0300

    HM_08_task1

---
+++ Solution/HM_08_task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HM_08_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите стихотворение в одну строку разделяя строки точкой с запятой");
+
+            string s = Console.ReadLine();
+            string s2 = s.Replace('о', 'а');
+            string[] mass = s2.Split(';');
+
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.WriteLine(mass[i]);
+            }
+        }
+    }
+}

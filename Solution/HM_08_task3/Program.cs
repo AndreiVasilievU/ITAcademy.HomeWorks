f8dae58c27f357180135cd4582b7c3ea24219304
Андрей Васильев <puhlikvinni@gmail.com>
2020-04-03 14:54:51 +0300

    hm_08

---
+++ Solution/HM_08_task3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace HM_08_task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char k1 = '.';
+            char k2 = '!';
+            char k = '?';
+            string str = "1a!2.3!!.. 4.!.?6 7! ..?";
+            int indexMin = str.IndexOf(k);
+
+            string str1 = str.Substring(indexMin);
+            string str2 = str.Substring(0, indexMin);
+
+            StringBuilder sb1 = new StringBuilder(str1);
+            sb1.Replace(" ", "_");
+            StringBuilder sb2 = new StringBuilder(str2);
+            for(int i=0 ; i < sb2.Length; i++)
+            {
+                if(sb2[i].Equals('.') || sb2[i].Equals('!'))
+                {
+                    sb2.Remove(i, 1);
+                    i--;
+                }
+            }
+
+            Console.WriteLine(sb1);
+            Console.WriteLine(sb2);
+        }
+    }
+}

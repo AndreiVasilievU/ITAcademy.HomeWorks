762bf313f725a80be75c17112fbdef20afbaca97
Андрей Васильев <puhlikvinni@gmail.com>
2020-04-17 14:36:56 +0300

    hm_09_task_1

---
+++ Solution/AssemblyOne/SportBike.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssemblyOne
+{
+    class SportBike : Motorcycle
+    {
+
+    }  
+}
---
+++ Solution/AssemblyTwo/Program.cs
@@ -0,0 +1,22 @@
+﻿using AssemblyOne;
+using System;
+
+namespace AssemblyTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Motorcycle moto = new Motorcycle();
+
+            //видны только паблик модификаторы из другой сборки
+            Console.WriteLine(Motorcycle.MinSpeedPublic);
+
+            moto.MovePublic();
+
+            moto.MaxSpeedPublic = 10;
+
+            //а наследника невидно из другой сборки вообще, у Motorcycle - publik, у SportBike - internal
+        }
+    }
+}

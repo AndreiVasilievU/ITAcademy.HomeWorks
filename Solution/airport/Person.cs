677a9e05ac5363fc95524a4693bc933c3ad754de
Андрей Васильев <puhlikvinni@gmail.com>
2020-04-18 17:59:55 +0300

    hm_9_task_2

---
+++ Solution/airport/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace airport
+{
+    class Person
+    {
+        public string name { get; set; }
+        public string isTicket = "да";
+        public string isBigBag = "да";
+        public string isSmallBag = "да";
+
+        public Person()
+        {
+        }
+    }
+}
---
+++ Solution/airport/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace airport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+
+            ChekIn ch = new ChekIn();
+            Security sec = new Security();
+            PassControl pas = new PassControl();
+
+            ch.CheckTicket(p);
+            ch.CheckBaggage(p);
+
+            sec.CheckSmallBag(p);
+            sec.GoToPassControl(p);
+
+            pas.PassportControl(p);
+        }
+    }
+}
